d5ece04d1f29eb0e2041377c3afb2a9a6d637fab
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-05-08 11:57:24 +0100

    Initial repo setup.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import css from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css.App}>
+      <header className={css.appHeader}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={css.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"

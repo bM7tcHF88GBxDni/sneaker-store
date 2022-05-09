cd544e197f3ecff63971daf0786ead741936d9ff
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-05-09 15:50:16 +0100

    Add dropdown animation to logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useSpring, animated, config } from "@react-spring/web";
 
 import css from "./Header.module.css";
 
 function Header() {
+  const dropdown = useSpring({
+    from: { position: "relative", bottom: "15vh" },
+    to: { position: "relative", bottom: "0vh" },
+    config: config.gentle,
+  });
+
   return (
-    <>
+    <animated.div style={dropdown}>
       <div className={css.logoContainer}>
         <img
           src="./sole.svg"
@@ -12,7 +19,7 @@ function Header() {
           className={css.logo}
         />
       </div>
-    </>
+    </animated.div>
   );
 }
 

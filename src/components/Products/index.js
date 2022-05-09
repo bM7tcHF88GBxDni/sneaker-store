bef4820229f15936f6dd68f6497ae759ed50e09d
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-05-09 15:40:17 +0100

    Add mounting animation to Products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,10 +1,17 @@
+import { useSpring, animated } from "@react-spring/web";
+
 import data from "../../data.js";
 import Product from "../Product/index.js";
 import css from "./Products.module.css";
 
 function Products() {
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: [{ opacity: 1 }, { opacity: 0.8 }, { opacity: 1 }],
+  });
+
   return (
-    <>
+    <animated.div style={fadeIn}>
       <div className={css.productsContainer}>
         {
           /* Map through sneakers data here */
@@ -15,7 +22,7 @@ function Products() {
           })
         }
       </div>
-    </>
+    </animated.div>
   );
 }
 
